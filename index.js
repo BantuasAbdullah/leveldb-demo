b4fd95ac1fdc6e5add4ef31eee0ab89ebc89defd
Lucman Abdulrachman <fencim@gmail.com>
2021-02-09 13:23:46 +0800

    put value

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var level = require('level');
 
 var db = level("./test-db");
+
+db.put("activity", "demo level db",  function (err) {
+  if (err) {
+    console.log("opss", err);
+  } else  {
+    console.log("saved");
+  }
+})
